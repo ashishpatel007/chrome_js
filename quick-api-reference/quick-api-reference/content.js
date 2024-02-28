194261a672f1143db869e36601a1e867661531c8
Ashish Patel <ashpat86@gmail.com>
2024-02-28 07:57:00 -0500

    updated omnibox

---
+++ quick-api-reference/quick-api-reference/content.js
@@ -0,0 +1,54 @@
+/*
+(async () => {
+    // Sends a message to the service worker and receives a tip in response
+    const { tip } = await chrome.runtime.sendMessage({ greeting: 'tip' });
+
+    const nav = document.querySelector('.upper-tabs > nav');
+
+    const tipWidget = createDomElement(
+        '<button type="button" popovertarget="tip-popover" popovertargetaction="show" ' +
+        'style="padding: 0 12px; height: 36px;">' +
+        '<span style="display: block; font: var(--devsite-link-font, 500 14px/20px var(--devsite-primary-font-family));">Tip</span>' +
+        '</button>'
+    );
+
+    const popover = createDomElement(
+        '<div id="tip-popover" popover style="margin: auto;"> ${tip}</div>'
+    );
+
+    document.body.append(popover);
+    nav.append(tipWidget);
+})();
+
+function createDomElement(html) {
+    const dom = new DOMParser().parseFromString(html, 'text/html');
+    return dom.body.firstElementChild;
+}
+
+*/
+
+// Popover API https://chromestatus.com/feature/5463833265045504
+
+(async () => {
+    const nav = document.querySelector('.upper-tabs > nav');
+
+    const { tip } = await chrome.runtime.sendMessage({ greeting: 'tip' });
+
+    const tipWidget = createDomElement(`
+    <button type="button" popovertarget="tip-popover" popovertargetaction="show" style="padding: 0 12px; height: 36px;">
+      <span style="display: block; font: var(--devsite-link-font,500 14px/20px var(--devsite-primary-font-family));">Tip</span>
+    </button>
+  `);
+
+    const popover = createDomElement(
+        `<div id='tip-popover' popover style="margin: auto;">${tip}</div>`
+    );
+
+    document.body.append(popover);
+    nav.append(tipWidget);
+})();
+
+function createDomElement(html) {
+    const dom = new DOMParser().parseFromString(html, 'text/html');
+    return dom.body.firstElementChild;
+}
